9644eefd5181aa273871680d7e7f403e9e24aadc
Pritam <52597351+14pritam@users.noreply.github.com>
2021-07-10 14:32:52 +0530

    Update infixtopostfix.c

--- infixtopostfix.c
+++ infixtopostfix.c
@@ -1,175 +1,66 @@
-
 #include<stdio.h>
-#include<string.h>
-char stack[50];
-int top=-1;
-void post(char infix[]);
-void push(char);
-char pop();
+#include<ctype.h>
 
-void main()
+char stack[100];
+int top = -1;
+
+void push(char x)
 {
-    char infix[25];
-    printf("\nENTER THE INFIX EXPRESSION = ");
-    gets(infix);
-    post(infix);
-    getch();
+    stack[++top] = x;
 }
 
-void push(char symb)
-{
-    if(top>=49)
-    {
-          printf("\nSTACK OVERFLOW");
-         getch();
-         return;
-    }
-    else
-    {
-          top=top+1;
-         stack[top]=symb;
-    }
-}
 char pop()
 {
-     char item;
-     if(top==-1)
-     {
-            printf("\nSTACK IS EMPTY");
-            getch();
-            return(0);
-      }
-      else
-     {
-            item=stack[top];
-            top--;
-     }
-     return(item);
+    if(top == -1)
+        return -1;
+    else
+        return stack[top--];
 }
-int preced(char ch)
+
+int priority(char x)
 {
-      if(ch==47)
-      {
-             return(5);
-      }
-      else if(ch==42)
-      {
-            return(4);
-      }
-      else if(ch==43)
-      {
-             return(3);
-      }
-      else
-      return(2);
-}
-void post(char infix[])
-{
-      int l;
-      int index=0,pos=0;
-      char symbol,temp;
-      char postfix[40];
-      l=strlen(infix);
-      push('#');
-      while(index<l)
-      {
-             symbol=infix[index];
-             switch(symbol)
-             {
-                    case '(': push(symbol);
-                    break;
-                    case ')': temp=pop();
-                    while(temp!='(')
-                    {
-                            postfix[pos]=temp;
-                            pos++;
-                            temp=pop();
-                    }
-                    break;
-                    case '+':
-                    case '-':
-                    case '*':
-                    case '/':
-                    case '^':
-                    while(preced(stack[top])>=preced(symbol))
-                    {
-                            temp=pop();
-                            postfix[pos]=temp;
-                            pos++;
-                    }
-                    push(symbol);
-                    break;
-                    default: postfix[pos++]=symbol;
-                    break;
-            }
-            index++;
-      }
-      while(top>0)
-      {
-               temp=pop();
-               postfix[pos++]=temp;
-      }
-       postfix[pos++]='\0';
-       puts(postfix);
-       return;
-}
-
-
-    return 0;
-}*/
-
-int precedence(char k) {
-if(k=='^')
-    return 3;
-else if(k=='*' || k=='/')
-    return 2;
-else if(k=='+' || k=='-')
-    return 1;
-else
+    if(x == '(')
+        return 0;
+    if(x == '+' || x == '-')
+        return 1;
+    if(x == '*' || x == '/')
+        return 2;
     return 0;
 }
 
-
-void inftopost(char exp[20]) {
-int i=0;
-while(exp[i]!=';') {
-        if(isalpha(exp[i]) || isdigit(exp[i]))
-                   printf("%c",exp[i]);
-        else if(exp[i]=='(')
-        push('(');
-        else if(exp[i]=='^' || exp[i]=='+' || exp[i]=='-' || exp[i]=='/' || exp[i]=='*' )
+int main()
+{
+    char exp[100];
+    char *e, x;
+    printf("Enter the expression : ");
+    scanf("%s",exp);
+    printf("\n");
+    e = exp;
+    
+    while(*e != '\0')
+    {
+        if(isalnum(*e))
+            printf("%c ",*e);
+        else if(*e == '(')
+            push(*e);
+        else if(*e == ')')
         {
-            if(stack[top]=='(' || precedence(exp[i])>precedence(stack[top]))
-                push(exp[i]);
-            else {
-            while(precedence(stack[top])>=precedence(exp[i])) {
-                if(stack[top]!='(') {
-                printf("%c",stack[top]);
-                pop();
-                }
-                else
-                break;
-            }
-          }
+            while((x = pop()) != '(')
+                printf("%c ", x);
         }
-        else if(exp[i]==')')
+        else
         {
-            while(stack[top]!='(') {
-                    printf("%c",stack[top]);
-                    pop();
-            }
-        pop();
+            while(priority(stack[top]) >= priority(*e))
+                printf("%c ",pop());
+            push(*e);
         }
+        e++;
+    }
+    
+    while(top != -1)
+    {
+        printf("%c ",pop());
+    }return 0;
+}
 
-i++;
-}
-}
-
-int main() {
-char in_arr[20];
-printf("\nEnter infix expression to be evaluated\t");
-gets(in_arr);
-inftopost(in_arr);
-printf("\n");
-return 1;
-}
+//input: a+b*c
